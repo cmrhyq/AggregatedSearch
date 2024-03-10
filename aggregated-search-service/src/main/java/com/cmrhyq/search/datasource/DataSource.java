c1368533988b773f8302d65889814ce2251154be
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-03-10 23:55:43 +0800

    1.聚合搜索接口  - 新增适配器模式接口、适配器模式方法实现

---
+++ aggregated-search-service/src/main/java/com/cmrhyq/search/datasource/DataSource.java
@@ -0,0 +1,31 @@
+package com.cmrhyq.search.datasource;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.cmrhyq.search.model.entity.Picture;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * <p>数据源接口，适配器模式</p>
+ *
+ * @author Alan Huang
+ * @version v0.0.1
+ * @classname DataSource.java
+ * @project aggregated-search-service
+ * @package com.cmrhyq.search.datasource
+ * @date 2024/3/10 21:53
+ * @email cmrhyq@163.com
+ * @since v0.0.1
+ */
+public interface DataSource<T> {
+
+    /**
+     * 搜索门面模式接口
+     *
+     * @param searchText
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    Page<T> doSearch(String searchText, long pageNum, long pageSize) throws UnsupportedEncodingException;
+}
