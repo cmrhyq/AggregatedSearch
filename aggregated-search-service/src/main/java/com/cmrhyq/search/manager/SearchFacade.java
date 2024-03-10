2bebc7d23ddd9e4ee5afa4cfe341307c9a45664f
AlanHuang <63389034+cmrhyq@users.noreply.github.com>
2024-03-10 23:55:19 +0800

    1.聚合搜索接口  - 优化搜索门面模式类SearchFacade.java方法代码，新增适配器模式，用于解决switch过长的问题

--- aggregated-search-service/src/main/java/com/cmrhyq/search/manager/SearchFacade.java
+++ aggregated-search-service/src/main/java/com/cmrhyq/search/manager/SearchFacade.java
@@ -1,9 +1,8 @@
 package com.cmrhyq.search.manager;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.cmrhyq.search.common.BaseResponse;
 import com.cmrhyq.search.common.ErrorCode;
-import com.cmrhyq.search.common.ResultUtils;
+import com.cmrhyq.search.datasource.*;
 import com.cmrhyq.search.exception.BusinessException;
 import com.cmrhyq.search.exception.ThrowUtils;
 import com.cmrhyq.search.model.dto.post.PostQueryRequest;
@@ -14,17 +13,17 @@ import com.cmrhyq.search.model.enums.SearchTypeEnum;
 import com.cmrhyq.search.model.vo.PostVO;
 import com.cmrhyq.search.model.vo.SearchVo;
 import com.cmrhyq.search.model.vo.UserVO;
-import com.cmrhyq.search.service.PictureService;
-import com.cmrhyq.search.service.PostService;
-import com.cmrhyq.search.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -44,16 +43,20 @@ import java.util.concurrent.CompletableFuture;
 public class SearchFacade {
 
     @Resource
-    private PictureService pictureService;
+    private PictureDataSource pictureDataSource;
 
     @Resource
-    private PostService postService;
+    private UserDataSource userDataSource;
 
     @Resource
-    private UserService userService;
+    private PostDataSource postDataSource;
+
+    @Resource
+    private DataSourceRegistry dataSourceRegistry;
 
     /**
      * 聚合查询方法
+     *
      * @param searchRequest 接口请求参数
      * @param request       HttpRequest
      * @return BaseResponse<SearchVo>
@@ -64,11 +67,13 @@ public class SearchFacade {
         SearchTypeEnum searchTypeEnum = SearchTypeEnum.getEnumByValue(searchType);
         ThrowUtils.throwIf(StringUtils.isBlank(searchType), ErrorCode.PARAMS_ERROR);
         String searchText = searchRequest.getSearchText();
+        long current = searchRequest.getCurrent();
+        long pageSize = searchRequest.getPageSize();
         if (searchTypeEnum == null) {
             CompletableFuture<Page<Picture>> pictureTask = CompletableFuture.supplyAsync(() -> {
                 Page<Picture> picturePage = null;
                 try {
-                    picturePage = pictureService.searchPicture(searchText, 1, 10);
+                    picturePage = pictureDataSource.doSearch(searchText, current, pageSize);
                 } catch (UnsupportedEncodingException e) {
                     throw new RuntimeException(e);
                 }
@@ -78,14 +83,14 @@ public class SearchFacade {
             CompletableFuture<Page<UserVO>> userTask = CompletableFuture.supplyAsync(() -> {
                 UserQueryRequest userQueryRequest = new UserQueryRequest();
                 userQueryRequest.setUserName(searchText);
-                Page<UserVO> userVOPage = userService.listUserVoByPage(userQueryRequest);
+                Page<UserVO> userVOPage = userDataSource.doSearch(searchText, current, pageSize);
                 return userVOPage;
             });
 
             CompletableFuture<Page<PostVO>> postTask = CompletableFuture.supplyAsync(() -> {
                 PostQueryRequest postQueryRequest = new PostQueryRequest();
                 postQueryRequest.setSearchText(searchText);
-                Page<PostVO> postVOPage = postService.listPostVoByPage(postQueryRequest, request);
+                Page<PostVO> postVOPage = postDataSource.doSearch(searchText, current, pageSize);
                 return postVOPage;
             });
 
@@ -105,25 +110,9 @@ public class SearchFacade {
             }
         } else {
             SearchVo searchVo = new SearchVo();
-            switch (searchTypeEnum){
-                case POST:
-                    PostQueryRequest postQueryRequest = new PostQueryRequest();
-                    postQueryRequest.setSearchText(searchText);
-                    Page<PostVO> postVOPage = postService.listPostVoByPage(postQueryRequest, request);
-                    searchVo.setPostList(postVOPage.getRecords());
-                    break;
-                case USER:
-                    UserQueryRequest userQueryRequest = new UserQueryRequest();
-                    userQueryRequest.setUserName(searchText);
-                    Page<UserVO> userVOPage = userService.listUserVoByPage(userQueryRequest);
-                    searchVo.setUserList(userVOPage.getRecords());
-                    break;
-                case PICTURE:
-                    Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
-                    searchVo.setPictureList(picturePage.getRecords());
-                    break;
-                default:
-            }
+            DataSource<?> dataSource = dataSourceRegistry.getDataSourceByType(searchType);
+            Page<?> page = dataSource.doSearch(searchText, current, pageSize);
+            searchVo.setDataList(page.getRecords());
             return searchVo;
         }
     }
